c6bfce45a68b9895c45377cd1e59ad3e6f66e7c2
rutuja2606 <rutujapise26@gmail.com>
2021-03-03 12:18:53 +0530

    updated for front-end

--- ppmtoolapi/src/main/java/com/cg/ppmtoolapi/PpmtoolapiApplication.java
+++ ppmtoolapi/src/main/java/com/cg/ppmtoolapi/PpmtoolapiApplication.java
@@ -2,8 +2,13 @@ package com.cg.ppmtoolapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@CrossOrigin
 public class PpmtoolapiApplication {
 
 	public static void main(String[] args) {
